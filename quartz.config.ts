2538d37419e7ad1c46852496f29bf658bdbd26c6
sirolf-otrebla <albeflor@gmail.com>
2023-10-28 16:54:30 +0200

    change title

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "⚔️ Swirlenheim Wiki ⚔️",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "swirlenheim-wiki.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
